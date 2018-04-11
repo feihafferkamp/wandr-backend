b74c306ea3e1a52bbae75449e9bcb977a39c8a49
f3igao <f3igao@icloud.com>
2018-04-11 10:35:51 -0400

    fix edit activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,7 +2,9 @@ class ActivitiesController < ApplicationController
   before_action :set_activity, only: [:show, :update, :destroy]
 
   def index
-    @activities = Activity.where(trip_id: params[:trip_id])
+    # on client side, user_trip is just trip
+    trip = UserTrip.find(params[:trip_id]).trip
+    @activities = Activity.where(trip_id: trip.id)
     render json: @activities
   end
 
@@ -12,7 +14,8 @@ class ActivitiesController < ApplicationController
 
   def create
     destination = Destination.find_or_create_by(destination_params)
-    trip = Trip.find_or_create_by(trip_params)
+    user_trip = UserTrip.find(params[:user_trip][:id])
+    trip=user_trip.trip
     @activity = Activity.new(activity_params)
     @activity.trip_id = trip.id
     @activity.destination_id = destination.id
@@ -22,13 +25,6 @@ class ActivitiesController < ApplicationController
       render json: {errors: @activity.errors.full_messages}, status: 422
     end
   end
-
-  # @trip = Trip.find_or_create_by(trip_id)
-  # @user_trip = UserTrip.new(user_trip_params)
-  # @user_trip.user_id = user_in_session.id
-  # @user_trip.trip_id = @trip.id
-  # @user_trip.save
-  # render json: {trip: TripSerializer.new(@trip), user_trip: UserTripSerializer.new(@user_trip)}
 
   def update
     if @activity.update(params[:activity])
@@ -55,9 +51,5 @@ class ActivitiesController < ApplicationController
 
   def destination_params
     params.require(:destination).permit(:name, :description, :lat, :lng)
-  end
-
-  def trip_params
-    params.require(:trip).permit(:name, :description, :duration)
   end
 end
