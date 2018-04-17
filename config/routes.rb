c6116650c2662470b44a19e091378d3aaeccb1a9
f3igao <f3igao@icloud.com>
2018-04-16 23:59:10 -0400

    set basic action cable

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,14 @@ Rails.application.routes.draw do
   post "/friendships/update", to: "friendships#update"
   post "/friendships/cancel", to: "friendships#cancel"
   delete "/friendships/destroy", to: "friendships#destroy"
-
   # resources :tags, except: [:new, :edit, :destroy]
-
   post "/signup", to: "users#create"
   post "/login", to: "auth#create"
   get "/current_user", to: "auth#show"
+  resources :chatrooms
+  post '/chatrooms/:chatroom_id/authorize', to: 'chatrooms#open'
+  post '/chatrooms/:chatroom_id/message', to: 'chatrooms#message'
+  # post '/chatrooms/delete_message', to: 'chatrooms#delete_message'
+
+  mount ActionCable.server => '/cable'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180413031452) do
+ActiveRecord::Schema.define(version: 20180417034719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20180413031452) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "destinations", force: :cascade do |t|
     t.citext "name"
     t.text "description"
@@ -46,6 +53,16 @@ ActiveRecord::Schema.define(version: 20180413031452) do
     t.boolean "accepted", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id"
+    t.bigint "chatroom_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "notes", force: :cascade do |t|
@@ -119,4 +136,6 @@ ActiveRecord::Schema.define(version: 20180413031452) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
 end
