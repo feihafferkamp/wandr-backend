cbed6ce526e8fd90bd39b0c21995ac8c3a42de5e
f3igao <f3igao@icloud.com>
2018-04-24 11:56:22 -0400

    take out byebug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,6 @@ class UsersController < ApplicationController
 
   def add_message
     receiver = User.find_by(id: params[:id])
-    byebug
     if user_in_session && receiver
       message = Message.create(sender: user_in_session, receiver: receiver, content: params[:content])
       UserChannel.broadcast_to(receiver, prepare_message(message))
